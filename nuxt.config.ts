d3deb1ab8b9e0976afa85e4aaf51eb65b4012e25
moises <elnaufrago2009@gmail.com>
2026-08-20 01:12:43 -0300

    conecta supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
+  modules: ['@nuxtjs/supabase'],
+  supabase: {
+    redirect: false,
+  },
 })
